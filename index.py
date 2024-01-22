1349728622f86cbde870fa352da03d26a84d4761
fikreanteneh <fikremariam.anteneh@aait.edu.et>
2024-01-22 10:23:13 +0300

    Bug Fix

--- index.py
+++ index.py
@@ -10,9 +10,6 @@ from utils import column_to_letter
 from dotenv import load_dotenv
 from datetime import datetime
 
-from google.oauth2.service_account import Credentials
-import gspread
-
 import pymongo
 from pymongo.server_api import ServerApi
 import json
@@ -21,43 +18,22 @@ from bson import json_util
 
 load_dotenv('.env')
 
-
 MAIN_SHEETNAME = os.getenv("MAIN_SHEET_NAME")
-
-# gc = gspread.service_account(filename=os.getenv("GOOGLE_CREDENTIALS"))
-
-scopes = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive",
-]
-
-credentials = Credentials.from_service_account_info(
-    json.loads(os.getenv("GOOGLE_CREDENTIALS")), scopes=scopes
-)
-
-gc = gspread.authorize(credentials)
-
 git_client_id = os.getenv("GITHUB_CLIENT_ID")
 git_client_secret = os.getenv("GITHUB_CLIENT_SECRET")
-
 mongo_client = pymongo.MongoClient(os.getenv("MONGODB_CONNECTION_STRING"), server_api=ServerApi('1'))
 db = mongo_client[os.getenv("MONGODB_DB_NAME")]
-
-
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}}, supports_credentials=True)
 api = Api(app)
-
-
 def parse_json(data):
     return json.loads(json_util.dumps(data))
 
-def push_to_sheet(sheetName, cellReference, gitUrl, attempts, timeTaken, timeTakenCell):
+def push_to_sheet(sheetName, studentName, gitUrl, attempts, timeTaken, questionColumn, timespentColumn):
     url = (
         f"https://script.google.com/macros/s/{os.getenv('SHEET_APPSCRIPT_DEPLOYMENT')}/exec"
-        + f"?sheetName={sheetName}&cellReference={cellReference}&gitUrl={gitUrl}&attempts={attempts}&timeTaken={timeTaken}$timeTakenCell={timeTakenCell}"
+        + f"?sheetName={sheetName}&studentName={studentName}&gitUrl={gitUrl}&attempts={attempts}&timeTaken={timeTaken}&questionColumn={questionColumn}&timespentColumn={timespentColumn}"
     )
-
     requests.get(url)
 
 
@@ -99,25 +75,18 @@ def api():
         "questionUrl",
         "platform",
     ]
-
     for atr in attribs:
         if atr not in json:
             return f"{atr} not found", 400
-
-    # Push to mongodb
-    student_collection = db.People
-    question_collection = db.Questions
-
-    student = student_collection.find_one({"Name": json["studentName"]})
-    question = question_collection.find_one({"URL": json["questionUrl"]})
+        
+    student = db.People.find_one({"Name": json["studentName"]})
+    question = db.Questions.find_one({"URL": json["questionUrl"]})
 
     if not student:
         return "Student not found", 400
-
     if not question:
         return "Question not found", 400
-
-    sh = gc.open(MAIN_SHEETNAME)
+    
 
     interaction = {
         "Column": question["Column"],
@@ -132,30 +101,17 @@ def api():
     }
 
     db.Interactions.insert_one(interaction)
-
-    # Attach to google sheet
-    ws = sh.worksheet(question["Sheet"])
-
-    studentNames = ws.col_values(1)
-
-    studentRow = None
-    for row, name in enumerate(studentNames):
-        if name == student["Name"]:
-            studentRow = row + 1
-            break
-    else:
-        return "Student not found on sheet", 400
-
     questionColumn = column_to_letter(question["Column"])
     timespentColumn = column_to_letter(question["Column"] + 1)
 
-    push_to_sheet(
-        question["Sheet"],
-        f"{questionColumn}{studentRow}",
+    push_to_sheet( 
+        question["Sheet"], 
+        json["studentName"], 
         json["gitUrl"],
         json["attempts"],
         json["timeTaken"],
-        f"{timespentColumn}{studentRow}",
+        questionColumn,
+        timespentColumn,
     )
     return jsonify({"status": "OK"})
 
