c6511d3cf39ac6a3c51f77c80f7b3ef5e3a0e4db
fikreanteneh <fikremariam.anteneh@aait.edu.et>
2024-01-25 15:22:46 +0300

    JS Version

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import cors from 'cors';
-// import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 import express from 'express';
 import { MongoClient } from 'mongodb';
 import { columnToLetter } from './utils.js';
 
-// dotenv.config();
+dotenv.config();
 
 const MAIN_SHEETNAME = process.env.MAIN_SHEET_NAME;
 const gitClientId = process.env.GITHUB_CLIENT_ID;
@@ -122,13 +122,14 @@ app.post('/api', async (req, res) => {
 });
 
 
-
-
-app.listen(3000, async () => {
+const starter = async () => { 
     await mongoClient.connect();
-    if (!mongoClient.isConnected()) {
-        console.error("MongoDB connection failed!");
-        process.exit(1);
-    }
     db = mongoClient.db(process.env.MONGODB_DB_NAME);
-});
+    console.log(db)
+    app.listen(3000, () => {
+        console.log("Server started on port 3000");
+    });
+}
+
+
+starter();
